d9b5b644691e97ea3c1140195cbef09e1c2684ad
redark6 <redark66@gmail.com>
2021-02-01 08:56:58 +0100

    first commit spring

---
+++ src/main/java/fr/montreuil/iut/csid/group/alpha/projetBilleterieSpring/ProjetBilleterieSpringApplication.java
@@ -0,0 +1,13 @@
+package fr.montreuil.iut.csid.group.alpha.projetBilleterieSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetBilleterieSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetBilleterieSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/montreuil/iut/csid/group/alpha/projetBilleterieSpring/ProjetBilleterieSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.montreuil.iut.csid.group.alpha.projetBilleterieSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetBilleterieSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

23513c653f23b58e435f5023808cf28301e97449
Milutin-Stan <milutinstan@outlook.fr>
2021-05-20 17:24:01 +0200

    changelog 2.1 et bugfix

--- src/main/resources/liquibase/changelog-v2.sql
+++ src/main/resources/liquibase/changelog-v2.sql
@@ -1,1 +1,15 @@
 --liquibase formatted sql
+
+--changeset nvoxland:1
+create table rating (
+    id SERIAL PRIMARY KEY,
+    user_id varchar(50),
+    event_id int,
+    rating integer,
+     CONSTRAINT fk_user_id_rating
+          FOREIGN KEY(user_id)
+    	  REFERENCES users(username),
+     CONSTRAINT fk_event_id_rating
+           FOREIGN KEY(event_id)
+           REFERENCES events(id)
+);
